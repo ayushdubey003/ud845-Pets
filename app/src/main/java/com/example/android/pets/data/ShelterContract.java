62ac8e6855297548fbf4b28af2d1f1c37be1e9f4
ayushdubey003 <codeayush1998@gmail.com>
2019-01-22 23:47:58 +0530

    Reformat Code

--- app/src/main/java/com/example/android/pets/data/ShelterContract.java
+++ app/src/main/java/com/example/android/pets/data/ShelterContract.java
@@ -2,19 +2,21 @@ package com.example.android.pets.data;
 
 import android.provider.BaseColumns;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
 public final class ShelterContract {
     private ShelterContract() {
     }
 
     public static final class PetsEntry implements BaseColumns {
-        public final String TABLE = BaseColumns._ID;
-        public final String ID = "_id";
-        public final String NAME = "name";
-        public final String GENDER = "gender";
-        public final String BREED = "breed";
-        public final String WEIGHT = "weight";
-        public final int MALE = 1;
-        public final int FEMALE = 2;
-        public final int UNKNOWN = 0;
+        public static final String TABLE = "pets";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+        public static final int GENDER_UNKNOWN = 0;
     }
 }
